a742e5af9f59a16f7e1e4099f6b529a5dd6d014c
Eric Lindvall <eric@5stops.com>
2008-07-08 10:12:39 -0700

    Initial commit

---
+++ dialplan.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+
+# 
+# You can call in to this at (206) 357-6220 x12455
+#
+default do
+  begin
+    timeline = open('http://twitter.com/statuses/user_timeline/MarsPhoenix.json').read
+    timeline = ActiveSupport::JSON.decode(timeline)
+    timeline.reject! { |status| status['text'].match(/^@/) }
+
+    text = timeline.first['text'].gsub('"', "'")
+
+    execute 'swift', %{"Callie^#{text}"}
+  rescue 
+    execute 'swift', %{"Sorry, we weren't able to get the status for the Mars Phoenix project."}
+    raise
+  end
+end
